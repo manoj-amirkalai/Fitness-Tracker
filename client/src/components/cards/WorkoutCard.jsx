c91be0412294260f8fed5bd359e94fdc6b50e944
Manoj.A <manojamir2911@gmail.com>
2024-05-20 12:29:07 +0530

    commit

--- client/src/components/cards/WorkoutCard.jsx
+++ client/src/components/cards/WorkoutCard.jsx
@@ -2,21 +2,21 @@ import { FitnessCenterRounded, TimelapseRounded } from "@mui/icons-material";
 import React from "react";
 import styled from "styled-components";
 
-// const Card = styled.div`
-//   flex: 1;
-//   min-width: 250px;
-//   max-width: 400px;
-//   padding: 16px 18px;
-//   border: 1px solid ${({ theme }) => theme.text_primary + 20};
-//   border-radius: 14px;
-//   box-shadow: 1px 6px 20px 0px ${({ theme }) => theme.primary + 15};
-//   display: flex;
-//   flex-direction: column;
-//   gap: 6px;
-//   @media (max-width: 600px) {
-//     padding: 12px 14px;
-//   }
-// `;
+const Card = styled.div`
+  flex: 1;
+  min-width: 250px;
+  max-width: 400px;
+  padding: 16px 18px;
+  border: 1px solid ${({ theme }) => theme.text_primary + 20};
+  border-radius: 14px;
+  box-shadow: 1px 6px 20px 0px ${({ theme }) => theme.primary + 15};
+  display: flex;
+  flex-direction: column;
+  gap: 6px;
+  @media (max-width: 600px) {
+    padding: 12px 14px;
+  }
+`;
 // const Category = styled.div`
 //   width: fit-content;
 //   font-size: 14px;
